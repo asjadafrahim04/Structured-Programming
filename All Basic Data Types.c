df8ae1749ac51288b132820df30fbef10e89a738
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-17 00:46:04 +0600

    Add files via upload

---
+++ All Basic Data Types.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int x=50;
+  printf("You entered an integer: %d\n",x);
+float y=6.12345;
+  printf("You entered a float number : %0.3f\n",y);
+char ch='s';
+  printf("You entered a character: %c\n", ch);
+double z=2.34;
+  printf("You entered a double number : %lf\n",z);
+return 0;
+}
+
