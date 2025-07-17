15727d1592b82016d4785adf7ff4cc65dd8c3e6a
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-17 19:45:37 +0600

    Add files via upload

---
+++ Constent And Symbolic Constent.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ //symbolic constants
+    #define PI 3.1416
+    #define GRAVITY 9.81
+    int main(){
+        printf("%0.4f\n",PI);
+    printf("%0.2f\n",GRAVITY);
+            printf("\n");
+
+    // Constants
+    const int int_contant = 75;
+    printf("%d\n",int_contant);
+    const float float_contant = 6.753;
+    printf("%.2f\n",float_contant);
+    return 0;
+}
