f3ee18308ccdfbfab4a8817e815a84cf8d2cbfbc
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-18 23:11:23 +0600

    Add files via upload

---
+++ Arithmetic Operations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int number1=6,number2=8;
+ 
+    int addition = number1 + number2;
+    int subtraction = number1 - number2;
+    int multiplication = number1 * number2;
+    int divisor = number1 / number2;
+    int remainder = number1 % number2;
+
+    printf("addition %d\n subtraction %d\n multiplication %d\n divisor %d\n remainder %d\n",addition,subtraction,multiplication,divisor,remainder);
+    return 0;
+}
