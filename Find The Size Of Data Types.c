f8cf2b3f43ad5008f8dd9c1ab65045168700e2bd
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-20 02:03:07 +0600

    Add files via upload

---
+++ Find The Size Of Data Types.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  printf("Size of int data type : %d\n", sizeof(int));
+  printf("Size of long long int data type : %d\n", sizeof(long long int));
+  printf("Size of float data type : %d\n", sizeof(float));
+  printf("Size of double data type : %d\n", sizeof(double));
+  printf("Size of char data type : %d\n", sizeof(char));
+ return 0;
+}
