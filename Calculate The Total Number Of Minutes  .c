fa64a560d47cd2350f577e6a029d46303c79eee5
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-22 01:53:28 +0600

    Add files via upload

---
+++ Calculate The Total Number Of Minutes  .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main(){
+     int hour=2;
+     int minutes=30;
+     
+
+
+     int hourtominutes = hour*60;
+     int totalminutes = hourtominutes+minutes;
+
+     printf("Total minutes : %d\n",totalminutes);
+     return 0;
+ }
