61b0271a8737e2e7b717b20c2793ca90e4f60d8d
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-28 20:39:51 +0600

    Basic Q14

---
+++ Input ASCII & Display Caracter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//ASCII number and display appropriate
+characters on screen
+int main(){
+    int ascii;
+    scanf("%d",&ascii);
+
+    char ch = ascii;
+        printf("%c",ch);
+    return 0;
+}
